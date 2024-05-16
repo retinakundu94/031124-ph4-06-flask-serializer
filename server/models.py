01e0e92ff54d018ffe568f49596123c9b8ebd8f7
Retina Kundu <retinakundu94@gmail.com>
2024-05-16 11:20:08 -0400

    addchanges

--- server/models.py
+++ server/models.py
@@ -11,7 +11,7 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Doctor(db.Model):
+class Doctor(db.Model, SerializerMixin):
     
     __tablename__ = 'doctors_table'
 
@@ -19,18 +19,39 @@ class Doctor(db.Model):
     name = db.Column(db.String, nullable=False)
     specialty = db.Column(db.String)
 
+    appointments = db.relationship('Appointment', back_populates='doctor')
 
-class Patient(db.Model):
+    patients = association_proxy('appointments', 'patient')
+
+    serialize_rules=["-appointments", "patients", "-patients.doctors"]
+
+    
+
+
+class Patient(db.Model, SerializerMixin):
     
     __tablename__ = 'patients_table'
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
 
+    appointments = db.relationship('Appointment', back_populates='patient')
 
-class Appointment(db.Model):
+    doctors = association_proxy('appointments', 'doctor')
+
+    serialize_rules = ("-appointments", "doctors", "-doctors.patients")
+
+
+class Appointment(db.Model, SerializerMixin):
     
     __tablename__ = 'appointments_table'
 
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('doctors_table.id'))
+    patient_id = db.Column(db.Integer, db.ForeignKey('patients_table.id'))
+
+    patient = db.relationship('Patient', back_populates='appointments')
+    doctor = db.relationship('Doctor', back_populates='appointments' )
+
+    serialize_rules = ["doctor.appointments", "patient.appointments"]
